e91eb5a43723d4d721fd8b771b666be476d2c926
Caitlyn Jameson <jamesoca@oregonstate.edu>
2021-10-26 21:38:51 -0500

    Caitlyn first commit- added Navbar component and logo

---
+++ src/components/navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Navbar} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
+import Logo from '../images/PM_black-small.png';
+
+
+
+class NavBar extends React.Component{
+
+    render(){
+        return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+            <Navbar.Brand><img className="Logo" src = {Logo} alt="PetMate"/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav>
+                    <Nav.Link>News</Nav.Link>
+                    <Nav.Link>Profile</Nav.Link>
+                    <Nav.Link>Logout</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
+            )
+    }
+};
+
+
+
+export default NavBar;

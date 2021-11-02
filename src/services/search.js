c27f1b666b5d1679821c287a95d79309d9cdac84
kaquino1 <59567712+kaquino1@users.noreply.github.com>
2021-11-02 00:09:19 -0400

    fixed server URL

--- src/services/search.js
+++ src/services/search.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/search'
+const baseUrl = 'https://scary-vault-59712.herokuapp.com/pets'
 
 const getAll = (searchString) => {
     return axios.get(`${baseUrl}/${encodeURI(searchString)}`).then(response => response.data);

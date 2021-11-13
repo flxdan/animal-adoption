24f216df818d63467634d56dc9897779fcb3e8c7
kaquino1 <59567712+kaquino1@users.noreply.github.com>
2021-11-13 12:24:21 -0500

    added gravatars to user profiles

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 
 import ErrorAlert from './ErrorAlert';
-
+import md5 from 'md5';
 import AuthService from "../services/authService";
 
 const User = () => {
@@ -12,21 +12,26 @@ const User = () => {
 
     if (!currentUser) { return <ErrorAlert message={'Status 401: Not Authorized'} /> }
     else {
+        const hashedEmail = md5(currentUser.email.trim().toLowerCase())
         return (
             <Container className='mt-5'>
                 
-                <Col xl={6} className='mx-auto'>
-                <Card>
-                    <h3 className='mb-4'> User Profile </h3>
-                    <Card.Text>
+                <Col lg={4} className='mx-auto'>
+                <Card className='user-card'>
+                    <h3 className='mb-4 text-center'> User Profile </h3>
+                    <div className='text-center'>
+                    <img className='avatar' src={`https://gravatar.com/avatar/${hashedEmail}?s=200&d=identicon`} alt='user avatar' />
+                    </div>
+                    
+                    <Card.Text className='fw-bold text-center'>
+                        {currentUser.username} 
+                    </Card.Text>
+                    <Card.Text className='text-center'>
+                       <span className='fw-bold'>Email:</span> {currentUser.email} 
+                    </Card.Text>  
+                    <Card.Text className='text-center'>
                         <span className='fw-bold'>Account Type:</span> {currentUser.roles.includes('ROLE_ADMIN') ? 'Admin' : 'Public'} 
                     </Card.Text>
-                    <Card.Text>
-                        <span className='fw-bold'>Username:</span> {currentUser.username} 
-                    </Card.Text>
-                    <Card.Text>
-                       <span className='fw-bold'>Email:</span> {currentUser.email} 
-                    </Card.Text>     
                 </Card>
                 </Col>
                 <h3>Favorites: </h3>

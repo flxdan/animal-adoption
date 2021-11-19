9f6e71f5b325b6c8bc477589f7aa2cfa17ee7abe
Caitlyn Jameson <jamesoca@oregonstate.edu>
2021-11-19 13:31:34 -0600

    updated homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,67 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { Carousel, Container, Row, Col } from "react-bootstrap";
+import image1 from '../images/cat-slide.jpg';
+import image2 from '../images/dog-slide.jpg';
+import image3 from '../images/hamster-slide.jpg';
+import newsService from '../services/news.js';
+import NewsItem from "./news_list_item";
+
+
+
+const DisplayNews = (props) => {
+  const content = props.news_array.map((news, index) => {
+    return (
+      <Col className = "d-flex">
+        <NewsItem
+        key={index}
+        headline={news.headline}
+        content={news.blurb}
+        date = {news.dateAdded}
+      ></NewsItem>
+      </Col>
+      
+    );
+  });
+  
+  return <>{content}</>;
+};
 
 const Home = () => {
 
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    newsService.getAll().then(response => {
+      console.log(response)
+      setNews(response);
+    });
+  }, []);
+
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h1> Home page </h1>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Carousel>
+          <Carousel.Item>
+            <img className = "d-block w-100" src={image1} alt="First Slide"/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className = "d-block w-100" src={image2} alt="First Slide"/>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className = "d-block w-100" src={image3} alt="First Slide"/>
+          </Carousel.Item>
+        </Carousel>
+        </Row>
+      <Row className="center">
+        <h1>Welcome</h1>
+        <p>
+          Here at PetMate, our goal is to make finidng your forever easy by using your prefences to help you mathc up iwth a compatible pet. We have all kinds of types, breeds, and dipositions available. 
+        </p>
+      </Row>
+      <Row>
+        <DisplayNews news_array = {news}/>
+      </Row>
+    </Container>
   );
 };
 

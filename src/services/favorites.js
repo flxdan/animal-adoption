1665365892a710f7062256d4aa7a154fcd70d782
Daniel Felix <felixda@oregonstate.edu>
2021-11-19 14:07:42 -0500

    fixed backend url

--- src/services/favorites.js
+++ src/services/favorites.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/users";
+const API_URL = "https://scary-vault-59712.herokuapp.com/users";
 
 const addFavorite = (userID, data) => {
     return axios.put(`${API_URL}/${userID}/favorites`, data).then(response => response.data);
